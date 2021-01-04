04cde070c1ad99295b4c3f29f907b7f050b5f00b
emiljapelt <ejapelt@hotmail.com>
2021-01-04 15:55:52 +0100

    Update ProshopSearcher to account for avalibility

--- Searchers/ProshopSearcher.cs
+++ Searchers/ProshopSearcher.cs
@@ -24,11 +24,25 @@ namespace Searchers
                 else 
                     readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
 
-                string pattern = @"""Price"": (\d+\.\d+),";
+                string pricepattern = @"""Price"": (\d+\.\d+),";
+                string statuspattern = @"<div class=""site-stock-text site-inline"">(.+?)(,|-|–).+<\/div>";
                 string data = readStream.ReadToEnd();
-                var matches = Regex.Matches(data, pattern);
-                if (matches.Count == 0) throw new Exception("Non-product page on supported webshop");
-                return float.Parse(matches[0].Groups[1].Value, CultureInfo.InvariantCulture);
+
+                var pricematches = Regex.Matches(data, pricepattern);
+                if (pricematches.Count == 0) throw new Exception("Non-product page on supported webshop");
+
+                var status = Regex.Matches(data, statuspattern)[0].Groups[1].Value;
+                switch(status)
+                {
+                    case "På lager ":
+                    case "Fjernlager":
+                        break;
+                    case "Bestillingsvare":
+                    case "Bestilt ":
+                        throw new Exception("Product not available");
+                }
+
+                return float.Parse(pricematches[0].Groups[1].Value, CultureInfo.InvariantCulture);
             }
             return null;
         }
