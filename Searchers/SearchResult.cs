a1a4fe6061c44daa8b432e037461e6147ff76d74
emiljapelt <ejapelt@hotmail.com>
2021-01-08 10:19:33 +0100

    Remove comment from SearchResult

--- Searchers/SearchResult.cs
+++ Searchers/SearchResult.cs
@@ -3,7 +3,6 @@ namespace Searchers
 {
     public class SearchResult
     {
-        //public bool IsProduct { get; set; }
         public bool? IsAvailable { get; set; }
         public float? Price { get; set; }
     }
